b2b35c0e964580701c9d039c110da7ad05398dae
Alex Seif <alex.seif@gmail.com>
2019-10-20 16:54:06 +0200

    Slot form

--- src/BackendBundle/Admin/SlotAdmin.php
+++ src/BackendBundle/Admin/SlotAdmin.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace BackendBundle\Admin;
 
@@ -9,49 +9,73 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 final class SlotAdmin extends AbstractAdmin
 {
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
-    {
-        $datagridMapper
-            ->add('name')
-            ->add('startAt')
-            ->add('endAt')
-        ;
-    }
 
-    protected function configureListFields(ListMapper $listMapper): void
-    {
-        $listMapper
-            ->add('name')
-            ->add('startAt')
-            ->add('endAt')
-            ->add('_action', null, [
-                'actions' => [
-                    'show' => [],
-                    'edit' => [],
-                    'delete' => [],
-                ],
-            ])
-        ;
-    }
+  protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
+  {
+    $datagridMapper
+        ->add('name')
+        ->add('startAt')
+        ->add('endAt')
+    ;
+  }
 
-    protected function configureFormFields(FormMapper $formMapper): void
-    {
-        $formMapper
-            ->add('name')
-            ->add('startAt')
-            ->add('endAt')
-        ;
-    }
+  protected function configureListFields(ListMapper $listMapper): void
+  {
+    $listMapper
+        ->add('name')
+        ->add('startAt')
+        ->add('endAt')
+        ->add('_action', null, [
+          'actions' => [
+            'show' => [],
+            'edit' => [],
+            'delete' => [],
+          ],
+        ])
+    ;
+  }
 
-    protected function configureShowFields(ShowMapper $showMapper): void
-    {
-        $showMapper
-            ->add('name')
-            ->add('startAt')
-            ->add('endAt')
-        ;
-    }
+  protected function configureFormFields(FormMapper $formMapper): void
+  {
+    $formMapper
+        ->add('name')
+        ->add('startAt')
+        ->add('endAt')
+        ->add('instrument', EntityType::class, [
+          'class' => \AppBundle\Entity\Instrument::class,
+          'query_builder' => function(\AppBundle\Repository\InstrumentRepository $er) {
+            return $er->queryEnabled();
+          },
+          'multiple' => false
+        ])
+        ->add('room', EntityType::class, [
+          'class' => \AppBundle\Entity\Room::class,
+          'query_builder' => function(\AppBundle\Repository\RoomRepository $er) {
+            return $er->queryEnabled();
+          },
+          'multiple' => false
+        ])
+        ->add('tutor', EntityType::class, [
+          'class' => \AppBundle\Entity\Tutor::class,
+          'query_builder' => function(\AppBundle\Repository\TutorRepository $er) {
+            return $er->queryEnabled();
+          },
+          'multiple' => false
+        ])
+    ;
+  }
+
+  protected function configureShowFields(ShowMapper $showMapper): void
+  {
+    $showMapper
+        ->add('name')
+        ->add('startAt')
+        ->add('endAt')
+    ;
+  }
+
 }
