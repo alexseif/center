7f431d16e1b2993f109286fb5f98ff7588b1ca1b
Alex Seif <alex.seif@gmail.com>
2019-11-24 12:58:56 +0200

    Customer

---
+++ src/AppBundle/Entity/Customer.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Customer
+ *
+ * @ORM\Table(name="customer")
+ * @ORM\Entity
+ */
+class Customer extends BaseUser
+{
+
+  /**
+   * @var int
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
+
+  /**
+   * @var int|null
+   *
+   * @ORM\Column(name="discount", type="integer", nullable=true)
+   */
+  private $discount;
+
+  /**
+   * Get id.
+   *
+   * @return int
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * Set name.
+   *
+   * @param string $name
+   *
+   * @return Customer
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  /**
+   * Get name.
+   *
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * Set email.
+   *
+   * @param string $email
+   *
+   * @return Customer
+   */
+  public function setEmail($email)
+  {
+    $this->email = $email;
+
+    return $this;
+  }
+
+  /**
+   * Get email.
+   *
+   * @return string
+   */
+  public function getEmail()
+  {
+    return $this->email;
+  }
+
+  /**
+   * Set mobile.
+   *
+   * @param string $mobile
+   *
+   * @return Customer
+   */
+  public function setMobile($mobile)
+  {
+    $this->mobile = $mobile;
+
+    return $this;
+  }
+
+  /**
+   * Get mobile.
+   *
+   * @return string
+   */
+  public function getMobile()
+  {
+    return $this->mobile;
+  }
+
+  /**
+   * Set discount.
+   *
+   * @param int|null $discount
+   *
+   * @return Customer
+   */
+  public function setDiscount($discount = null)
+  {
+    $this->discount = $discount;
+
+    return $this;
+  }
+
+  /**
+   * Get discount.
+   *
+   * @return int|null
+   */
+  public function getDiscount()
+  {
+    return $this->discount;
+  }
+
+}
