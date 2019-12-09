1d0a671f38b8c39ad0fc96c3ff065d0552d028cc
Alex Seif <alex.seif@gmail.com>
2019-12-09 14:45:59 +0200

    composer require simshaun/recurr

--- src/BackendBundle/Controller/AvailabilityAdminController.php
+++ src/BackendBundle/Controller/AvailabilityAdminController.php
@@ -5,12 +5,17 @@ declare(strict_types = 1);
 namespace BackendBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CRUDController;
+use Timeslot\Timeslot;
+use Timeslot\TimeslotCollection;
 
 final class AvailabilityAdminController extends CRUDController
 {
 
   public function availabilityAction()
   {
+    $dm = $this->getDoctrine()->getManager();
+    $timeslot = Timeslot::create($start, 0, 45);
+    $collection = TimeslotCollection::create($timeslot, 18);
     return $this->renderWithExtraParams('BackendBundle::availability.html.twig');
   }
 
